f9edf1432d3a38c36e7f8bcd48e0859f1c8d6ec9
Lauren Tan <poteto@users.noreply.github.com>
2016-06-13 16:59:49 -0400

    Fix missing exports (#17)

--- addon/validators/index.js
+++ addon/validators/index.js
@@ -2,3 +2,5 @@ export { default as validatePresence } from './presence';
 export { default as validateLength } from './length';
 export { default as validateNumber } from './number';
 export { default as validateFormat } from './format';
+export { default as validateInclusion } from './inclusion';
+export { default as validateExclusion } from './exclusion';
