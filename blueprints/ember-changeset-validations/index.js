f450eca6163c9032fdb2b095c90498a42addbf79
Lauren Tan <poteto@users.noreply.github.com>
2016-10-24 16:59:19 -0700

    Bump ember-changeset to 1.2.0 (#105)

--- blueprints/ember-changeset-validations/index.js
+++ blueprints/ember-changeset-validations/index.js
@@ -5,6 +5,6 @@ module.exports = {
   description: 'Install ember-changeset, a dependency of ember-changeset-validations',
   normalizeEntityName: function() {},
   afterInstall: function() {
-    return this.addAddonToProject('ember-changeset', '^1.1.4');
+    return this.addAddonToProject('ember-changeset', '^1.2.0');
   }
 };
