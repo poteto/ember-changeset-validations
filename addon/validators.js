bc862663077ee23268df72a9119fa9c7b19b0b1d
Bernardo Rittmeyer <brittmeyer@bittitan.com>
2017-01-24 10:26:23 -0800

    Fix validators import not working properly

---
+++ addon/validators.js
@@ -0,0 +1,7 @@
+export { default as validatePresence } from './validators/presence';
+export { default as validateLength } from './validators/length';
+export { default as validateNumber } from './validators/number';
+export { default as validateFormat } from './validators/format';
+export { default as validateInclusion } from './validators/inclusion';
+export { default as validateExclusion } from './validators/exclusion';
+export { default as validateConfirmation } from './validators/confirmation';
--- addon/validators/index.js
+++
@@ -1,7 +0,0 @@
-export { default as validatePresence } from './presence';
-export { default as validateLength } from './length';
-export { default as validateNumber } from './number';
-export { default as validateFormat } from './format';
-export { default as validateInclusion } from './inclusion';
-export { default as validateExclusion } from './exclusion';
-export { default as validateConfirmation } from './confirmation';
