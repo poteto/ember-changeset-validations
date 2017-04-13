7e0b63c3dea9bb308e4235b691f6eb6986d32dbe
Jason Tu <jasontu4@gmail.com>
2017-04-12 23:15:44 -0400

    Add `and` validator

---
+++ addon/validators/and.js
@@ -0,0 +1,39 @@
+import isPromise from 'ember-changeset/utils/is-promise';
+import Ember from 'ember';
+
+function notTrue(value) {
+  return Ember.typeOf(value) !== 'boolean' || !value;
+}
+
+function handleResult(result) {
+  if (notTrue(result)) throw result;
+  return true;
+}
+
+/**
+ * Accepts an array of ember-changeset-validations
+ * validation functions.
+ */
+export default function and(...validators) {
+  return (key, newValue, oldValue, changes, object) => {
+    for (let i = 0; i < validators.length; i++) {
+      const validation = validators[i](key, newValue, oldValue, changes, object);
+
+      if (isPromise(validation)) {
+        let promise = validation.then(handleResult);
+
+        for (let j = i+1; j < validators.length; j++) {
+          promise = promise
+            .then(() => validators[j](key, newValue, oldValue, changes, object))
+            .then(handleResult);
+        }
+
+        return promise.catch(err => err);
+      }
+
+      if (notTrue(validation)) return validation;
+    }
+
+    return true;
+  }
+}
