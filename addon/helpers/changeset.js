4dc93513e63fca692caf8d21732d15003cbf22fa
Austin Burdine <acburdine@gmail.com>
2017-05-31 17:50:58 -0400

    pass through skipValidate option in changeset helper
    no issue
    - with ember-changeset 1.3.0 the skipValidate option was added to the
    changeset object. While ember-changeset's helper was updated, this
    helper was not

--- addon/helpers/changeset.js
+++ addon/helpers/changeset.js
@@ -6,20 +6,20 @@ import isPromise from 'ember-changeset/utils/is-promise';
 
 const { Helper: { helper } } = Ember;
 
-export function changeset([model, validationMap]) {
+export function changeset([model, validationMap], options = {}) {
   if (isObject(validationMap)) {
     if (isPromise(model)) {
-      return model.then((resolved) => new Changeset(resolved, lookupValidator(validationMap), validationMap));
+      return model.then((resolved) => new Changeset(resolved, lookupValidator(validationMap), validationMap, options));
     }
 
-    return new Changeset(model, lookupValidator(validationMap), validationMap);
+    return new Changeset(model, lookupValidator(validationMap), validationMap, options);
   }
 
   if (isPromise(model)) {
-    return model.then((resolved) => new Changeset(resolved, validationMap));
+    return model.then((resolved) => new Changeset(resolved, validationMap, {}, options));
   }
 
-  return new Changeset(model, validationMap);
+  return new Changeset(model, validationMap, {}, options);
 }
 
 export default helper(changeset);
