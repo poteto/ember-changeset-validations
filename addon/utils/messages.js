14600656a2176f51095b14cd511bf89b7c08efa4
brandynbennett <brandynbennett@users.noreply.github.com>
2017-07-09 15:44:01 -0600

    Fix #139. `presence` messages were backwards. (#140)
    `buildMessage('present')` was returning `{key} must be blank` instead
    of `{key} can't be blank`.
    
    `buildMessage('blank')` was returning `{key} can't be blank` instead
    of `{key} must be blank`.

--- addon/utils/messages.js
+++ addon/utils/messages.js
@@ -9,6 +9,10 @@ const assign = Ember.assign || Ember.merge;
 const Messages = assign({}, _Messages);
 
 export default assign(Messages, {
+  // Blank and present are flipped in ember-validators. Need to flip them back here
+  blank: _Messages.present,
+  present: _Messages.blank,
+
   getDescriptionFor(key = '') {
     return capitalize(dasherize(key).split(/[_-]/g).join(' '));
   }
