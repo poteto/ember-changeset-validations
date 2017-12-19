1f53e0b4b48cb195949f4816e53d945b389a73be
Timofey Dergachev <t@exeto.me>
2017-12-19 22:27:01 +0300

    Fix regex for custom messages module (#160)

--- addon/utils/get-messages.js
+++ addon/utils/get-messages.js
@@ -1,11 +1,12 @@
 /* globals requirejs, requireModule */
 import Ember from 'ember';
+import config from 'ember-get-config';
 import defaultMessages from 'ember-changeset-validations/utils/messages';
 import withDefaults from 'ember-changeset-validations/utils/with-defaults';
 
 const { A: emberArray, isPresent } = Ember;
 const { keys } = Object;
-const matchRegex = /validations\/messages$/gi;
+const moduleName = `${config.modulePrefix}/validations/messages`;
 
 let cachedRef = null;
 
@@ -26,7 +27,7 @@ export default function getMessages(moduleMap = requirejs.entries, useCache = tr
   }
 
   let moduleKey = emberArray(keys(moduleMap))
-    .find((module) => isPresent(module.match(matchRegex)));
+    .find((key) => key === moduleName);
 
   if (isPresent(moduleKey)) {
     // Merge the user specified messages with the defaults
