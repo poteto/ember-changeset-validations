0179433127348b8505d3b57ebcbeb30d104632e1
Scott Newcomer <snewcomer@users.noreply.github.com>
2019-11-04 21:21:25 -0800

    [MINOR] - changeset helper reexport (#198)

--- addon/helpers/changeset.js
+++ addon/helpers/changeset.js
@@ -1,23 +1,8 @@
 import { helper } from '@ember/component/helper';
-import Changeset from 'ember-changeset';
-import lookupValidator from 'ember-changeset-validations';
-import isObject from 'ember-changeset/utils/is-object';
-import isPromise from 'ember-changeset/utils/is-promise';
+import { changeset as EmberChangeset } from 'ember-changeset/helpers/changeset';
 
-export function changeset([model, validationMap], options = {}) {
-  if (isObject(validationMap)) {
-    if (isPromise(model)) {
-      return model.then((resolved) => new Changeset(resolved, lookupValidator(validationMap), validationMap, options));
-    }
-
-    return new Changeset(model, lookupValidator(validationMap), validationMap, options);
-  }
-
-  if (isPromise(model)) {
-    return model.then((resolved) => new Changeset(resolved, validationMap, {}, options));
-  }
-
-  return new Changeset(model, validationMap, {}, options);
+export function changeset([obj, validations], options) {
+  return EmberChangeset([obj, validations], options);
 }
 
 export default helper(changeset);
