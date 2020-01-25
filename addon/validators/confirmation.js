916083da42adb850c3472968772ec167c8faf2a4
Scott Newcomer <snewcomer@users.noreply.github.com>
2020-01-24 21:28:41 -0800

    Brandynbennett/confirmation bug (#219)

--- addon/validators/confirmation.js
+++ addon/validators/confirmation.js
@@ -1,9 +1,16 @@
 import buildMessage from 'ember-changeset-validations/utils/validation-errors';
 import { validate } from 'ember-validators';
+import { assign } from '@ember/polyfills';
+
 
 export default function validateConfirmation(options = {}) {
-  return (key, newValue, _oldValue, changes/*, _content*/) => {
-    let result = validate('confirmation', newValue, options, changes, key);
+  return (key, newValue, _oldValue, changes , content = {}) => {
+    // Combine the changes on top of the content so that we evaluate against both default values
+    // and valid changes. `changes` only has valid changes that have been made and won't include
+    // default values
+    let model = assign(content, changes);
+
+    let result = validate('confirmation', newValue, options, model, key);
     return (result === true) ? true : buildMessage(key, result);
   };
 }
