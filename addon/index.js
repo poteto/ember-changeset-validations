349b47ff280a2339187567c5045b26ca8fcc67af
snewcomer <snewcomer24@gmail.com>
2020-04-26 07:51:26 -0700

    Use validated-changeset lookup

--- addon/index.js
+++ addon/index.js
@@ -1,23 +1,3 @@
-import { isEmpty } from '@ember/utils';
-import { isArray } from '@ember/array';
-import wrapInArray from 'ember-changeset-validations/utils/wrap';
-import handleMultipleValidations from 'ember-changeset-validations/utils/handle-multiple-validations';
-import { isPromise } from 'validated-changeset';
+import { lookupValidator } from 'validated-changeset';
 
-export default function lookupValidator(validationMap = {}) {
-  return ({ key, newValue, oldValue, changes, content }) => {
-    let validator = validationMap[key];
-
-    if (isEmpty(validator)) {
-      return true;
-    }
-
-    if (isArray(validator)) {
-      return handleMultipleValidations(validator, { key, newValue, oldValue, changes, content });
-    }
-
-    let validation = validator(key, newValue, oldValue, changes, content);
-
-    return isPromise(validation) ? validation.then(wrapInArray) : [validation];
-  };
-}
+export default lookupValidator;
