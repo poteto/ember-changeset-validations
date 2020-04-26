e3fc5f6d7a018e0470244a515e0e744ebf89a24c
snewcomer <snewcomer24@gmail.com>
2020-04-25 19:00:47 -0700

    Export date from index.js

--- addon/validators/index.js
+++ addon/validators/index.js
@@ -1,3 +1,4 @@
+export { default as validateDate } from './date';
 export { default as validatePresence } from './presence';
 export { default as validateLength } from './length';
 export { default as validateNumber } from './number';
