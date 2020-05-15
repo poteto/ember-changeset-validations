7e965b52f7e50575ddefbfc1208bbdb1038b9ea2
Scott Newcomer <snewcomer24@gmail.com>
2020-05-15 07:39:34 -0700

    revert assign

--- addon/validators/confirmation.js
+++ addon/validators/confirmation.js
@@ -1,5 +1,6 @@
 import buildMessage from 'ember-changeset-validations/utils/validation-errors';
 import { validate } from 'ember-validators';
+import { assign } from '@ember/polyfills';
 
 
 export default function validateConfirmation(options = {}) {
@@ -7,7 +8,7 @@ export default function validateConfirmation(options = {}) {
     // Combine the changes on top of the content so that we evaluate against both default values
     // and valid changes. `changes` only has valid changes that have been made and won't include
     // default values
-    let model = Object.assign({}, content, changes);
+    let model = assign({}, content, changes);
 
     let result = validate('confirmation', newValue, options, model, key);
     return (result === true) ? true : buildMessage(key, result);
