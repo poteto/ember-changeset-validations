0d287982cb5360a734270a089006f222821a8d7d
frykten <johan.gache@concordnow.com>
2021-10-14 15:21:40 +0200

    Remove unuseful forcing of allowNone on the number validator

--- addon/validators/number.js
+++ addon/validators/number.js
@@ -5,10 +5,6 @@ import evValidateNumber from 'ember-validators/number';
 export default function validateNumber(options = {}) {
   options = withDefaults(options, { allowString: true, allowNone: false });
 
-  if (options.allowBlank) {
-    options.allowNone = true;
-  }
-
   return (key, value) => {
     let result = evValidateNumber(value, options, null, key);
     return result === true ? true : buildMessage(key, result);
