11ef2754154dc7247875eb0e4dc27b5f4e44e869
Sergey Astapov <SergeAstapov@gmail.com>
2021-10-27 11:21:23 -0400

    Remove changeset template helper re-export (#326)

--- addon/helpers/changeset.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { changeset as EmberChangeset } from 'ember-changeset/helpers/changeset';
-
-export function changeset([obj, validations], options) {
-  return EmberChangeset([obj, validations], options);
-}
-
-export default helper(changeset);
