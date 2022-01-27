9498698a15aad4e961d8614a026d6278458df439
Josemar Luedke <230476+josemarluedke@users.noreply.github.com>
2022-01-27 13:42:26 -0800

    Remove deprecated usage of assign (#339)

--- addon/validators/confirmation.js
+++ addon/validators/confirmation.js
@@ -1,13 +1,12 @@
 import buildMessage from 'ember-changeset-validations/utils/validation-errors';
 import evValidateConfirmation from 'ember-validators/confirmation';
-import { assign } from '@ember/polyfills';
 
 export default function validateConfirmation(options = {}) {
   return (key, newValue, _oldValue, changes, content = {}) => {
     // Combine the changes on top of the content so that we evaluate against both default values
     // and valid changes. `changes` only has valid changes that have been made and won't include
     // default values
-    let model = assign({}, content, changes);
+    let model = Object.assign({}, content, changes);
 
     let result = evValidateConfirmation(newValue, options, model, key);
     return result === true ? true : buildMessage(key, result);
